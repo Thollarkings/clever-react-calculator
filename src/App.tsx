e2f9ac0ad1ea162ff3fffd266b689bace55a9ce8
Thollarkings <97956738+Thollarkings@users.noreply.github.com>
2025-05-18 20:59:21 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/clever-react-calculator">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
